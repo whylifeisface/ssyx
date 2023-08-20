351e4f86d08b98b9af4134c552bae7fb451361b6
ss <com>
2023-08-20 15:23:01 +0800

    算是重新开始吧 之前错误太多 没有即使测试

---
+++ service/service-util/src/main/java/org/example/common/config/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package org.example.common.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("org.example.*.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor optimisticLockerInnerInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //向Mybatis过滤器链中添加分页拦截器
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
